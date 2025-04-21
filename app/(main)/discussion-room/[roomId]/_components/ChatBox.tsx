3105c887595a9e86ec897cdbb5bc8e874c63867e
Atibway <ainebyoonaatiidu@gmail.com>
2025-04-21 11:33:52 +0300

    dd

--- app/(main)/discussion-room/[roomId]/_components/ChatBox.tsx
+++ app/(main)/discussion-room/[roomId]/_components/ChatBox.tsx
@@ -28,45 +28,43 @@ export const ChatBox = ({
   summaryPage
 }: Props) => {
 const {roomId} = useParams();
-  const [loading, setLoading]= useState(false)
+const [loading, setLoading]= useState(false)
 
   const updateSummary = useMutation(api.DiscussionRoom.updateSummary)
 
   const GenerateFeedbackNotes = async () => {
-    setLoading(true);
-    // Call AI to generate feedback and notes
-    const result = await AIModelToGenerateFeedbackAndNotes({
-      coachingOption,
-      conversation,
-    });
-    console.log(!result.content);
-
-    if (!result?.content) {
-      toast.error("No content returned from AI model.")
-      setLoading(false);
-      throw new Error("No content returned from AI model.");
-    }else {
-      toast.success("content returned from AI model.")
-    }
-
-    // Update the summary in the backend
-  const summary =  await updateSummary({
-      id: roomId as Id<"DiscussionRoom">,
-      summary: result.content,
-    });
-
-    console.log(summary);
-    toast.success("Feedback/Notes Saved!")
-    setLoading(false);
-    // try {
+    try {
+      setLoading(true);
+      toast.success("starting generating...")
+      // Call AI to generate feedback and notes
+      const result = await AIModelToGenerateFeedbackAndNotes({
+        coachingOption,
+        conversation,
+      });
+      console.log(!result.content);
   
+      if (!result?.content) {
+        toast.error("No content returned from AI model.")
+        throw new Error("No content returned from AI model.");
+      }else {
+        toast.success("content returned from AI model.")
+      }
+  
+    //   // Update the summary in the backend
+    // const summary =  await updateSummary({
+    //     id: roomId as Id<"DiscussionRoom">,
+    //     summary: result.content,
+    //   });
+  
+    //   console.log(summary);
+    //   toast.success("Feedback/Notes Saved!")
 
-    // } catch (error) {
-    //   console.error("Failed to generate feedback notes:", error);
-    //   toast.error("Failed to generate feedback notes")
-    // } finally {
-    //   setLoading(false);
-    // }
+    } catch (error) {
+      console.error("Failed to generate feedback notes:", error);
+      toast.error("Failed to generate feedback notes")
+    } finally {
+      setLoading(false);
+    }
   };
   
 
